66d0c457d52d36dd44b4b0df3fab7c73ba9a96b7
Blacklucifer <c.marketsmueller@gmx.de>
2017-08-13 18:58:15 +0200

    Update Assignment0Test.java
    Test korrigiert

--- src/test/java/Assignment0Test.java
+++ src/test/java/Assignment0Test.java
@@ -18,7 +18,7 @@ public class Assignment0Test extends AbstractTimeoutKaraTest {
                         " ... ",
                         ".....",
                         " ... ",
-                        "     ",
+                        "  .  ",
                  });
     }
 
